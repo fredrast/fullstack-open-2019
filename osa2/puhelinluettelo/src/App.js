566549cca3f1bd3666ad4e4c7dbc3c6c9ef70869
Fredrik Åström <fredrik.astrom@iki.fi>
2019-05-03 19:50:04 +0300

    osa2/puhelinluettelo step5

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,35 +1,53 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Martti Tienari', number: '040-123456' },
-    { name: 'Arto Järvinen', number: '040-123456' },
-    { name: 'Lea Kutvonen', number: '040-123456' },
-  ]);
-  const [newName, setNewName] = useState('Syötä uusi nimi');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
+const FilterForm = ({ filter, setFilter }) => {
+  const isAlphanumeric = inputString => {
+    const allowedChars = /^[0-9 a-öA-Ö]+$/;
+    return inputString.match(allowedChars);
+  };
+  console.log('Rendering filter form...');
 
-  const rows = () =>
-    persons
-      .filter(person => person.name.match(new RegExp(filter, 'i')) != null)
-      .map(person => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ));
+  const handleFilterInputChange = event => {
+    const filterString = event.target.value;
+    console.log(filterString);
+    if (isAlphanumeric(filterString) || filterString === '') {
+      setFilter(filterString);
+    }
+  };
 
+  return (
+    <div>
+      rajaa näytettäviä{' '}
+      <input value={filter} onChange={handleFilterInputChange} />
+    </div>
+  );
+};
+
+const AddPersonForm = ({
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+  persons,
+  setPersons,
+}) => {
+  const handleNameInputChange = event => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+    console.log(persons);
+  };
+  const handleNumberInputChange = event => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
   const addPerson = event => {
     event.preventDefault();
     console.log('Executing addPerson...');
     console.log(newName);
     console.log(persons.indexOf(newName));
+    console.log(persons);
 
-    console.log('Checking if ' + newName + ' exists in list: ');
-    console.log(persons.find(person => (person.name = newName)));
-
-    if (persons.find(person => (person.name = newName)) == undefined) {
+    if (persons.find(person => person.name === newName) === undefined) {
       const personObject = {
         name: newName,
         number: newNumber,
@@ -44,27 +62,11 @@ const App = () => {
     }
   };
 
-  const handleNameInputChange = event => {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  };
-  const handleNumberInputChange = event => {
-    console.log(event.target.value);
-    setNewNumber(event.target.value);
-  };
-  const handleFilterInputChange = event => {
-    console.log(event.target.value);
-    setFilter(event.target.value);
-  };
-
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
-      rajaa näytettäviä{' '}
-      <input value={filter} onChange={handleFilterInputChange} />
       <form onSubmit={addPerson}>
         <div>
-          <h2>lisää uusi</h2>
+          <h3>lisää uusi</h3>
           nimi: <input value={newName} onChange={handleNameInputChange} />
         </div>
         <div>
@@ -74,8 +76,51 @@ const App = () => {
           <button type="submit">lisää</button>
         </div>
       </form>
-      <h2>Numerot</h2>
-      {rows()}
+    </div>
+  );
+};
+
+const PersonList = ({ persons, filter }) => {
+  console.log('Rendering PersonList...');
+  console.log(persons);
+  return persons
+    .filter(person => person.name.match(new RegExp(filter, 'i')) != null)
+    .map(person => <PersonEntry key={person.name} person={person} />);
+};
+
+const PersonEntry = ({ person }) => {
+  return (
+    <p>
+      {person.name} {person.number}
+    </p>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-223456' },
+    { name: 'Arto Järvinen', number: '040-323456' },
+    { name: 'Lea Kutvonen', number: '040-423456' },
+  ]);
+  const [newName, setNewName] = useState('Syötä uusi nimi');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <FilterForm filter={filter} setFilter={setFilter} />
+      <AddPersonForm
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        persons={persons}
+        setPersons={setPersons}
+      />
+      <h3>Numerot</h3>
+      <PersonList persons={persons} filter={filter} />
     </div>
   );
 };
